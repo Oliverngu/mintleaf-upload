64b262ef61576250b746637a151170c6f9b1cfbb
Oliverngu <138084319+Oliverngu@users.noreply.github.com>
2025-11-08 14:52:48 +0100

    fix: Update Firebase storage bucket URL
    The Firebase storage bucket URL in the configuration files has been updated to reflect the correct domain for Cloud Storage. This ensures proper integration with Firebase storage services.

--- src/core/firebase/config.ts
+++ src/core/firebase/config.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCB7ZTAhDlRwueGW6jqDdMqmpfHOI62mtE",
   authDomain: "mintleaf-74d27.firebaseapp.com",
   projectId: "mintleaf-74d27",
-  storageBucket: "mintleaf-74d27.appspot.com",
+  storageBucket: "mintleaf-74d27.firebasestorage.app",
   messagingSenderId: "1053273095803",
   appId: "1:1053273095803:web:84670303a5324c0d816cde",
   measurementId: "G-2Y86CZ0633"
